c50677eb95526d0a21386bd30a93bdcab8a448b5
Saulo <saulo.menches@gmail.com>
2026-08-15 22:37:46 -0300

    coloquei o texto com a imagem, mas ainda com erro a ser corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+
+import Hero from './components/Hero'
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Meu Portifólio</h1>
+      <Hero />
     </div>
   )
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,12 @@
+import './hero.css'
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <img src="/imagens/Design sem nome (1).png" alt="Avatar do Saulo" className="avatar-3d" />
+        <h1 className="hero-title">Hi, I'm Saulo</h1>
+      </div>
+    </section>
+  )
+}
